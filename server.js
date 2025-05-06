55c472730167d9e75fde2194faf1ed175fdfe40b
ShivaKrishnaReddyBurra <reddyburrashivakrishna@gmail.com>
2025-05-06 13:37:44 +0530

    update: added a test api

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/cart', cartRoutes);
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working' });
+}
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
